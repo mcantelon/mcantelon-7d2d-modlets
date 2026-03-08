7f7a282146395b8e67e9fb2feb84a7efdf4bdd4d
Mike Cantelon <mcantelon@gmail.com>
2026-03-07 19:22:53 -0800

    Add modlet

--- scripts/update_zip_files.sh
+++ scripts/update_zip_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 rm ZIPs/mcantelon-peaceful.zip
 cd modlets/mcantelon-peaceful
+ls -l
 zip -r ../../ZIPs/mcantelon-peaceful.zip .
+rm ../../ZIPs/mcantelon-traders-always-open.zip
+cd ../../modlets/mcantelon-traders-always-open
+ls -l
+zip -r ../../ZIPs/mcantelon-traders-always-open.zip .
